72701351e6d426f59ea885fd6a99c87af50c7c3b
mhmdntof <mohammad@gmail.com>
2026-05-21 17:06:35 +0300

    add procfile

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,13 @@ return Application::configure(basePath: dirname(__DIR__))
     ]);
 
     $middleware->statefulApi();
+
+$middleware->validateCsrfTokens(except: [
+            'api/login-web',
+            'login-web',
+            'api/login'
+        ]);
+
 })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(function ($request, $e) {
