1ea66c1897ad41aec82830b7aacdae2fec1d2bbb
mhmdntof <mohammad@gmail.com>
2026-06-03 18:23:36 +0300

    add procfile

--- app/Http/Controllers/PurchaseRequestController.php
+++ app/Http/Controllers/PurchaseRequestController.php
@@ -23,4 +23,72 @@ class PurchaseRequestController extends Controller
     Auth::user()
 );
     }
+
+
+
+    //تابع موافقة المدير
+
+    public function approveManager($id)
+{
+    $request = $this->purchaseRequestService->approveByManager($id);
+
+    return response()->json([
+        'message' => 'Request approved by manager',
+        'data' => $request
+    ]);
+}
+
+//تابع رفض المدير
+
+public function rejectManager(Request $request, $id)
+{
+    $request->validate([
+        'reason' => 'nullable|string'
+    ]);
+
+    $result = $this->purchaseRequestService->rejectByManager(
+        $id,
+        $request->reason
+    );
+
+    return response()->json([
+        'message' => 'Request rejected by manager',
+        'data' => $result
+    ]);
+}
+
+
+//موافقة رئيس لجنة الشراء 
+
+public function approveCommittee($id)
+{
+    $request = $this->purchaseRequestService->approveByCommittee($id);
+
+    return response()->json([
+        'message' => 'Request approved by committee',
+        'data' => $request
+    ]);
+}
+
+
+//تابع الرفض
+
+
+public function rejectCommittee(Request $request, $id)
+{
+    $request->validate([
+        'reason' => 'nullable|string'
+    ]);
+
+    $result = $this->purchaseRequestService->rejectByCommittee(
+        $id,
+        $request->reason
+    );
+
+    return response()->json([
+        'message' => 'Request rejected by committee',
+        'data' => $result
+    ]);
+}
+
 }
