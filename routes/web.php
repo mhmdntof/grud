67743a0ec4019b647d2dc3dcc7942b900c0e9387
mhmdntof <mohammad@gmail.com>
2026-06-14 15:08:49 +0300

    add procfile

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'loginWeb']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
