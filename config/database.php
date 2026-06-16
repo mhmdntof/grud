d20fb2fa82532b5ce7b02b7a3d90f443c1b2a33d
mhmdntof <mohammad@gmail.com>
2026-06-16 17:00:48 +0300

    add procfile

--- config/database.php
+++ config/database.php
@@ -84,10 +84,21 @@ return [
             ]) : [],
         ],
 
-     'pgsql' => [
+       'pgsql' => [
     'driver' => 'pgsql',
-    'url' => env('DATABASE_URL'),
+    'host' => env('DB_HOST'),
+    'port' => env('DB_PORT', 5432),
+    'database' => env('DB_DATABASE'),
+    'username' => env('DB_USERNAME'),
+    'password' => env('DB_PASSWORD'),
+
+    'charset' => 'utf8',
+    'prefix' => '',
+    'schema' => 'public',
+
+    'sslmode' => 'require',
 ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
