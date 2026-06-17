a9e8388356485b533e69cbeda3cb0e821ed771c7
mhmdntof <mohammad@gmail.com>
2026-06-17 14:44:50 +0300

    add procfile

--- app/Http/Requests/CreateEmployeeRequest.php
+++ app/Http/Requests/CreateEmployeeRequest.php
@@ -14,7 +14,7 @@ class CreateEmployeeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255',
+          //  'name' => 'required|string|max:255',
 
             'email' => 'required|email|unique:users,email',
 
