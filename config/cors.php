a4c5f17009a03aafdba5302ed482bed650c171e1
mhmdntof <mohammad@gmail.com>
2026-07-01 19:47:29 +0300

    add procfile

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://plaque-aid-49946997.figma.site'],
+    'allowed_origins' => [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+],
 
     'allowed_origins_patterns' => [],
 
