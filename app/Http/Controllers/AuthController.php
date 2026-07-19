9ceb14c2ffed8a02b1a5390a93523abf7d9a1fc8
mhmdntof <mohammad@gmail.com>
2026-07-19 20:34:06 +0300

    add procfile

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -51,7 +51,7 @@ class AuthController extends Controller
 
     return response()->json([
         'message' => 'Employee created successfully',
-        'otp' => $result['otp'],
+       
     ]);
 }
 
